cf50dcf665e7c2914a46ecd843de01bed4c47e89
Cameron Hall <22chall@wakatipu.school.nz>
2025-03-25 18:44:06 +1300

    v0.2.1
    menu system now includes addons to quesadillas, updated print system for better formatting

--- main.py
+++ main.py
@@ -2,31 +2,52 @@ BASIC = 10
 REGULAR = 12
 SUPERIOR = 16
 
-quesadillas = [["Plain Beef", BASIC, ["beef"]], 
-               ["Chicken", BASIC, ["chicken"]],
-               ["Vegetarian", BASIC, ["peppers", "beans"]],
-               ["Cheesy Beef", REGULAR, ["beef", "cheese"]],
-               ["Beef & Beans", REGULAR, ["beef", "beans"]],
-               ["Spicy Chicken", REGULAR, ["chicken", "jalapenos", "salsa"]],
-               ["Market Fish", REGULAR, ["fish", "capers"]],
-               ["The Pablo", SUPERIOR, ["chicken", "bacon", "jalapenos", "cheese", "salsa", "beans"]],
-               ["The Bass", SUPERIOR, ["fish", "capers", "chutney", "jalapenos"]],
+WIDTH = 33
+
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+RESET = "\033[0m"
+
+quesadillas = [["Plain Beef", BASIC, ["beef"], "Beef"], 
+               ["Chicken", BASIC, ["chicken"], "Chicken"],
+               ["Vegetarian", BASIC, ["peppers", "beans"], "Pepper and beans"],
+               ["Cheesy Beef", REGULAR, ["beef", "cheese"], "Beef and cheese"],
+               ["Beef & Beans", REGULAR, ["beef", "beans"], "Beef and beans"],
+               ["Spicy Chicken", REGULAR, ["chicken", "jalapenos", "salsa"], "Chicken, jalapenos and salsa"],
+               ["Market Fish", REGULAR, ["fish", "capers"], "Fish and capers"],
+               ["The Pablo", SUPERIOR, ["chicken", "beans", "jalapenos", "salsa"], "Chicken, beans, jalapenos and salsa"],
+               ["The Bass", SUPERIOR, ["fish", "capers", "jalapenos", "chutney"], "Fish, capers, jalapenos and chutney"],
+               ["The Joe", SUPERIOR, ["beef", "peppers", "cheese", "salsa"], "Beef, peppers, cheese and salsa"]
                ]
 
-def menu(incl_ingredients, no_incl_ingredients, incl_tiers, no_incl_tiers):
+extras = [[1, "salsa"],
+          [2, "cheese"],
+          [3, "beans", "peppers"],
+          [4, "capers", "chutney", "jalapenos"],
+          [5, "beef", "chicken"],
+          [6, "fish"]
+          ]
+
+
+def menu(incl_ingredients, no_incl_ingredients, incl_tiers, no_incl_tiers, incl_extras):
     current_menu = quesadillas 
-    print("MENU")
-    for i in range(len(current_menu)):
-        print(f"{current_menu[i][0]:>16} - {current_menu[i][1]} - ", end="")
-        for j in range(len(current_menu[i][2])):
-            if current_menu[i][2][-1] == current_menu[i][2][j]:
-                print(current_menu[i][2][j])
+    print(f"{BOLD + '           Q U E E N S T O W N   Q U E S A D I L L A S' + RESET:^73}")
+    print(f"{' '*(WIDTH+2)} |")
+    for i in range(0,len(current_menu),2):
+        print(f"\033[1m\033[4m{current_menu[i][0]:<{WIDTH}}{current_menu[i][1]}\033[0m | \033[1m\033[4m{current_menu[i+1][0]:<{WIDTH}}{current_menu[i+1][1]}\033[0m")
+        print(f"{current_menu[i][3]:<{WIDTH+2}} | {current_menu[i+1][3]:<{WIDTH+2}}\n{' '*(WIDTH+2)} |")
+
+    for i in range(len(extras)):
+        print(f"\033[1m\033[4m${extras[i][0]} EXTRAS{' '*(WIDTH-7)}\033[0m") #used special unicode char to work this
+        for j in range(len(extras[i])-1):
+            if extras[i][-1] == extras[i][j+1]:
+                print(f"{extras[i][j+1]}\n")
             else:
-                print(current_menu[i][2][j], end=", ")
-               
+                print(extras[i][j+1], end=", ")
 
 
 def main():
-    menu([],[],[],[])
+    menu([],[],[],[],bool)
+
 
 main()
