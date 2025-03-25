3861b708631716875d1c96d330a32c78e4be90f5
Cameron Hall <22chall@wakatipu.school.nz>
2025-03-25 15:19:52 +1300

    v0.2
    menu printing now works, different var is used as the list will be edited depending on user filters

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ quesadillas = [["Plain Beef", BASIC, ["beef"]],
                ["Cheesy Beef", REGULAR, ["beef", "cheese"]],
                ["Beef & Beans", REGULAR, ["beef", "beans"]],
                ["Spicy Chicken", REGULAR, ["chicken", "jalapenos", "salsa"]],
-               ["Market Fish", REGULAR, ["fish"]],
+               ["Market Fish", REGULAR, ["fish", "capers"]],
                ["The Pablo", SUPERIOR, ["chicken", "bacon", "jalapenos", "cheese", "salsa", "beans"]],
-               ["The Bass", SUPERIOR, ["fish", "capers", "chutney"]],
+               ["The Bass", SUPERIOR, ["fish", "capers", "chutney", "jalapenos"]],
                ]
 
 def menu(incl_ingredients, no_incl_ingredients, incl_tiers, no_incl_tiers):
-   current_menu = quesadillas 
-   print(current_menu)
+    current_menu = quesadillas 
+    print("MENU")
+    for i in range(len(current_menu)):
+        print(f"{current_menu[i][0]:>16} - {current_menu[i][1]} - ", end="")
+        for j in range(len(current_menu[i][2])):
+            if current_menu[i][2][-1] == current_menu[i][2][j]:
+                print(current_menu[i][2][j])
+            else:
+                print(current_menu[i][2][j], end=", ")
+               
 
 
 def main():
