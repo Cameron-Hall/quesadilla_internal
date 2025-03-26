da9b428dd46ea294f093e7d95e82d52d8fa96c79
Cameron Hall <22chall@wakatipu.school.nz>
2025-03-26 17:20:30 +1300

    v0.2.2
    removed extras from menu for now until i can figure out formatting, adjusted initial menu prints

--- main.py
+++ main.py
@@ -20,30 +20,23 @@ quesadillas = [["Plain Beef", BASIC, ["beef"], "Beef"],
                ["The Joe", SUPERIOR, ["beef", "peppers", "cheese", "salsa"], "Beef, peppers, cheese and salsa"]
                ]
 
-extras = [[1, "salsa"],
-          [2, "cheese"],
-          [3, "beans", "peppers"],
-          [4, "capers", "chutney", "jalapenos"],
-          [5, "beef", "chicken"],
-          [6, "fish"]
+extras = [[2, ["cheese", "salsa"], "cheese, salsa"],
+          [3, ["beans", "peppers"], "peppers, beans"],
+          [4, ["chutney", "jalapenos"], "chutney, jalapenos"],
+          [5, ["beef", "chicken"], "beef, chicken"]
           ]
 
 
 def menu(incl_ingredients, no_incl_ingredients, incl_tiers, no_incl_tiers, incl_extras):
     current_menu = quesadillas 
-    print(f"{BOLD + '           Q U E E N S T O W N   Q U E S A D I L L A S' + RESET:^73}")
+    print(f"{BOLD + '         Q U E E N S T O W N' + RESET:^73}")
+    print(f"{BOLD + '         Q U E S A D I L L A S' + RESET:^73}\n")
+    print(f"{BOLD + '         M E N U' + RESET:^73}")
     print(f"{' '*(WIDTH+2)} |")
     for i in range(0,len(current_menu),2):
         print(f"\033[1m\033[4m{current_menu[i][0]:<{WIDTH}}{current_menu[i][1]}\033[0m | \033[1m\033[4m{current_menu[i+1][0]:<{WIDTH}}{current_menu[i+1][1]}\033[0m")
         print(f"{current_menu[i][3]:<{WIDTH+2}} | {current_menu[i+1][3]:<{WIDTH+2}}\n{' '*(WIDTH+2)} |")
-
-    for i in range(len(extras)):
-        print(f"\033[1m\033[4m${extras[i][0]} EXTRAS{' '*(WIDTH-7)}\033[0m") #used special unicode char to work this
-        for j in range(len(extras[i])-1):
-            if extras[i][-1] == extras[i][j+1]:
-                print(f"{extras[i][j+1]}\n")
-            else:
-                print(extras[i][j+1], end=", ")
+    print()
 
 
 def main():
